791eb5eab980c8ca7834845ded798526c7f2885d
Brenda-Machado <brenda.silva.machado@grad.ufsc.br>
2023-05-09 16:26:00 -0300

    Estrutura ex03 prim e TO-DO

---
+++ Trabalho2/prim.py
@@ -0,0 +1,21 @@
+"""
+Trabalho 2 - Grafos
+Aluna: Brenda Silva Machado
+Exercício 3 - Algoritmo de Prim para Árvores Geradoras Mínimas
+"""
+
+""" TO-DO
+[] Encontrar um estrutura de dados adequada para Q (talvez árvore?)
+[] Implementar run()
+"""
+
+from Trabalho1.grafo import Grafo
+
+class Prim:
+    def __init__(self, grafo: Grafo):
+        self.grafo = grafo
+        self.A = []
+        self.Q = [] # ?
+ 
+    def run(self):
+        pass
